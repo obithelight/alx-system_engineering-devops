0c90336c7b4051764dd0ce14c538d555cf7d08d4
Obi Chukwu <kennetho.onu@gmail.com>
2024-04-24 12:08:36 +0100

    ssh client config with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
-#!/usr/bin/puppet
-# configures ssh with puppet
+#!/usr/bin/env puppet
+# using puppet to make changes to configuration file
 
 include stdlib
 
@@ -7,9 +7,5 @@ file_line { 'nopwd':
   ensure => present,
   line   => 'PasswordAuthentication no',
   path   => '/etc/ssh/ssh_config',
-}
-file_line { 'identity':
-  ensure => present,
   line   => 'IdentityFile ~/.ssh/school',
-  path   => '/etc/ssh/ssh_config',
 }
