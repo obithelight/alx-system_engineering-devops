38951db9dcb157e532515ecdb92536a7b5a553ba
Obi Chukwu <kennetho.onu@gmail.com>
2024-05-21 12:08:22 +0100

    export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,37 +1,28 @@
 #!/usr/bin/python3
-"""
-Using https://jsonplaceholder.typicode.com
-gathers data from API and exports it to JSON file
-Implemented using recursion
-"""
+"""A Python Module"""
 import json
-import re
 import requests
 import sys
 
 
-API = "https://jsonplaceholder.typicode.com"
-"""REST API url"""
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    user_id = int(sys.argv[1])
+    user_endpoint = "{}/users/{}".format(url, user_id)
+    username = requests.get(user_endpoint).json().get("username")
+    tasks_endpoint = "{}/todos".format(url)
+    tasks = requests.get(tasks_endpoint).json()
+    user_tasks = {
+        user_id: [
+            {
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": username,
+            }
+            for task in tasks if task.get("userId") == user_id
+        ]
+    }
 
-
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API, id)).json()
-            todos_res = requests.get('{}/todos'.format(API)).json()
-            user_name = user_res.get('username')
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            with open("{}.json".format(id), 'w') as json_file:
-                user_data = list(map(
-                    lambda x: {
-                        "task": x.get("title"),
-                        "completed": x.get("completed"),
-                        "username": user_name
-                    },
-                    todos
-                ))
-                user_data = {
-                    "{}".format(id): user_data
-                }
-                json.dump(user_data, json_file)
+    # Save file and convert to JSON
+    with open("{}.json".format(user_id), 'w', encoding="utf-8") as file:
+        json.dump(user_tasks, file)
