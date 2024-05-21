c176b936f78055855ff4b48fe75a6c0ffded3b12
Obi Chukwu <kennetho.onu@gmail.com>
2024-05-21 11:32:38 +0100

    export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,8 +1,10 @@
+'''
 #!/usr/bin/python3
 """A Python Module"""
 import json
 import requests
 import sys
+
 
 
 if __name__ == "__main__":
@@ -24,5 +26,29 @@ if __name__ == "__main__":
     }
 
     # save file and convert to json
+    with open("{}.json".format(user_id), 'w', encoding="utf-8") as file:
+        json.dump(user_tasks, file)
+'''
+
+#!/usr/bin/python3
+"""comment"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    user_id = int(sys.argv[1])
+    user_endpoint = "{}/users/{}".format(url, user_id)
+    username = requests.get(user_endpoint).json().get("username")
+    tasks_endpoint = "{}/todos".format(url)
+    tasks = requests.get(tasks_endpoint).json()
+    user_tasks = {user_id: [{"task": task.get("title"),
+                             "completed": task.get("completed"),
+                             "username": username}
+                             for task in tasks if task.get("userId") == user_id]
+                             }
+    
+    #save in json file
     with open("{}.json".format(user_id), 'w', encoding="utf-8") as file:
         json.dump(user_tasks, file)
