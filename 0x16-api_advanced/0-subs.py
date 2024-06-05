5e7a9b91b43031cf482665091a6bb84edb1de35c
Obi Chukwu <kennetho.onu@gmail.com>
2024-06-05 02:49:56 +0100

    script that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,6 +6,9 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    returns the number of subscribers
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     header = {"User-Agent": "Mozilla/5.0"}
 
