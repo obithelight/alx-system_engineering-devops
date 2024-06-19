2cb73714b3311409057848943d36348966dfbccf
kenneth Onu <kennetho.onu@gmail.com>
2024-06-19 04:35:05 +0000

    remove failed requests in nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,11 @@
 file { '/etc/default/nginx':
-	ensure => file,
-	content => "ULIMIT='-n 4096'\n",
+  ensure  => file,
+  content => "ULIMIT='-n 4096'\n",
+  notify  => Service['nginx'],  # Notify the Nginx service to restart if the file changes
+}
+
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/default/nginx'],  # Restart the service if the file changes
 }
