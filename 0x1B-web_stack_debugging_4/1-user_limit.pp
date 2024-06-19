0946c41f4fba75685f2a8c536c2517b6625946c8
kenneth Onu <kennetho.onu@gmail.com>
2024-06-19 05:24:56 +0000

    increase the limit of holberton user

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+# This Puppet script sets user limits for 'holberton' in the /etc/security/limits.conf file.
+exec { 'change value to 100':
+  command => "/bin/sed -i 's/5/100/g' /etc/security/limits.conf"
+}
+
+exec { 'change value to 80':
+  command => "/bin/sed -i 's/4/80/g' /etc/security/limits.conf"
+}
