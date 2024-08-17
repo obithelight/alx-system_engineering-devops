c2e3b922fd0e723e76da6770737b762620783616
Obi Chukwu <kennetho.onu@gmail.com>
2024-08-17 08:34:53 +0100

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
+"""Queries the `Reddit API`, returns the number of subscribers
+    of a subreddit
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    """
+    returns the number of subscribers for a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    header = {"User-Agent": "Mozilla/5.0"}
+
+    data = requests.get(url, headers=header, allow_redirects=False)
+
+    if data.status_code == 200:
+        subscribers = data.json().get("data").get("subscribers")
+        return subscribers
+    return 0
